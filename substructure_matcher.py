7c6b16cd2a78e56044def4bd764e95e8b3af13db
Mikel Emaldi <m.emaldi@gmail.com>
2013-10-30 17:14:27 +0100

    Initial commit

---
+++ substructure_matcher.py
@@ -0,0 +1,116 @@
+from os import listdir
+from alignapy import *
+from requests.exceptions import MissingSchema
+
+SUBS_DIR='/home/mikel/doctorado/subgraphs/subs/'
+
+class Graph():
+    def __init__(self):
+        self.vertex_list = []
+        self.edge_list = []
+        
+    def add_vertex(self, vertex):
+        self.vertex_list.append(vertex)
+        
+    def add_edge(self, edge):
+        self.edge_list.append(edge)
+
+class Vertex():
+    def __init__(self, vertex_id, name):
+        self.vertex_id = vertex_id
+        self.vertex_id = name
+
+class Edge():
+    def __init__(self, source, target, name):
+        self.source = source
+        self.target = target
+        self.name = name
+
+
+
+def get_base(uri):
+    if '#' in uri:
+        return uri.split('#')[0].replace('<', '') + '#'
+    else:
+        chunks = uri.split('/')
+        base = ''
+        for i in xrange(len(chunks) - 1):
+            base += chunks[i] + '/'
+        return base.replace('<', '')
+
+graph_dict = {}
+ontology_list = []
+for sub in listdir(SUBS_DIR):
+    f = open(SUBS_DIR + '/' + sub)
+    graph = Graph()
+    for line in f:
+        if line.find('v') == 0:
+            chunks = line.split(' ')
+            vertex_id = chunks[1]
+            vertex_name = chunks[2].replace('\n', '')
+            base = get_base(vertex_name)
+            if base not in ontology_list:
+                ontology_list.append(base)
+            vertex = Vertex(vertex_id, vertex_name)
+            graph.add_vertex(vertex)
+        elif line.find('d') == 0:
+            chunks = line.split(' ')
+            source = chunks[1]
+            target = chunks[2]
+            name = chunks[3].replace('"', '')
+            base = get_base(name)
+            if base not in ontology_list:
+                ontology_list.append(base)
+            edge = Edge(source, target, name)
+            graph.add_edge(edge)
+    graph_dict[sub] = graph
+    f.close()
+
+blacklist = []
+align_dict = {}
+for o1 in ontology_list:
+    for o2 in ontology_list:
+        if o1 not in blacklist and o2 not in blacklist and o1 != o2:
+            proceed = False
+            if o1 not in align_dict:
+                proceed = True
+                align_dict[o1] = {}
+                align_dict[o1][o2] = {}
+            elif o2 not in align_dict[o1]:
+                proceed = True
+                align_dict[o1][o2] = {}
+                
+            if proceed:
+                print o1, o2
+                #print align_dict
+                ap = EditDistNameAlignment()
+                try:
+                    ap.init(o1, o2)
+                    ap.align()
+                    print 'cells', len(ap.cell_list)
+                    '''for cell in ap.cell_list:
+                        print cell.prop1, cell.prop2, cell.measure'''
+                    align_dict[o1][o2]['NameAndPropertyAlignment'] = ap
+                except UriNotFound as e:
+                    blacklist.append(e.uri)
+                except MissingSchema as e:
+                    #print 'Incorrect schema'
+                    print e.args
+                except UnsupportedContent as e:
+                    print e
+                    blacklist.append(e.uri)
+                except IncorrectMimeType as e:
+                    print e
+                    blacklist.append(e.uri)
+                except Exception as e:
+                    pass
+    print blacklist
+                
+
+'''for key1 in graph_dict:
+    g1 = graph_dict[key1]
+    for key2 in graph_dict:
+        if key1 != key2:
+            g2 = graph_dict[key2]
+            for 
+'''
