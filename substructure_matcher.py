e69bad486d312fd55ef5e4becaf8dc7ad5586a66
Mikel Emaldi <m.emaldi@gmail.com>
2013-11-04 11:32:34 +0100

    Now using Pool.map

--- substructure_matcher.py
+++ substructure_matcher.py
@@ -4,9 +4,14 @@ from alignapy import *
 from requests.exceptions import MissingSchema
 import datetime
 from mongoengine import *
+import multiprocessing
+
+
 
 SUBS_DIR='/home/mikel/doctorado/subgraphs/subs/'
-ALIGN_LIST=['NameAndPropertyAlignment', 'StringDistAlignment', 'NameEqAlignment', 'EditDistNameAlignment', 'SMOANameAlignment', 'SubsDistNameAlignment', 'JWNLAlignment']
+#ALIGN_LIST=['NameAndPropertyAlignment', 'StringDistAlignment', 'NameEqAlignment', 'EditDistNameAlignment', 'SMOANameAlignment', 'SubsDistNameAlignment', 'JWNLAlignment']
+ALIGN_LIST=['JWNLAlignment']
+POOL_SIZE=10
 
 class MongoAlign(Document):
     source_onto = StringField(required=True)
@@ -58,6 +63,36 @@ def get_base(uri):
             base += chunks[i] + '/'
         return base.replace('<', '')
 
+def process((ap, o1, o2, align_class)):
+    try:
+        if len(BlackList.objects(ontology=o1)) <= 0 and len(BlackList.objects(ontology=o2)) <= 0 and o1 != o2:
+            alignment = MongoAlign.objects(source_onto=o1, target_onto=o2, align_class=align_class)
+            if len(alignment) <= 0:
+                ap.init(o1, o2)
+                ap.align()
+                print 'cells', len(ap.cell_list)
+                for cell in ap.cell_list:
+                    #print str(cell.prop1[0]), str(cell.prop2[0]), cell.measure
+                    try:
+                        mongo = MongoAlign(source_onto=o1, target_onto=o2, source_obj=str(cell.prop1[0]), target_obj=str(cell.prop2[0]), align_class=align_class, score=cell.measure)
+                        mongo.save()
+                    except Exception as e:
+                        print e
+    except UriNotFound as e:
+        blacklist_add(e.uri)
+    except MissingSchema as e:
+        #print 'Incorrect schema'
+        print e.args
+    except UnsupportedContent as e:
+        print e
+        blacklist_add(e.uri)
+    except IncorrectMimeType as e:
+        print e
+        blacklist_add(e.uri)
+    except Exception as e:
+        pass
+
+
 connect('alignment')
 
 graph_dict = {}
@@ -88,41 +123,23 @@ for sub in listdir(SUBS_DIR):
     graph_dict[sub] = graph
     f.close()
 
+ap_list = []
+
 for o1 in ontology_list:
     for o2 in ontology_list:
-        if len(BlackList.objects(ontology=o1)) <= 0 and len(BlackList.objects(ontology=o2)) <= 0 and o1 != o2:
-            #print o1, o2
-            #print align_dict
-            for align_class in ALIGN_LIST:
-                alignment = MongoAlign.objects(source_onto=o1, target_onto=o2, align_class=align_class)
-            if len(alignment) <= 0:
-                print 'Creating alignment between %s and %s using %s' % (o1, o2, align_class) 
-                ap = getattr(alignapy, align_class)()
-                try:
-                    ap.init(o1, o2)
-                    ap.align()
-                    print 'cells', len(ap.cell_list)
-                    for cell in ap.cell_list:
-                        #print str(cell.prop1[0]), str(cell.prop2[0]), cell.measure
-                        try:
-                            mongo = MongoAlign(source_onto=o1, target_onto=o2, source_obj=str(cell.prop1[0]), target_obj=str(cell.prop2[0]), align_class='EditDistNameAlignment', score=cell.measure)
-                            mongo.save()
-                        except Exception as e:
-                            print e
-                except UriNotFound as e:
-                    blacklist_add(e.uri)
-                except MissingSchema as e:
-                    #print 'Incorrect schema'
-                    print e.args
-                except UnsupportedContent as e:
-                    print e
-                    blacklist_add(e.uri)
-                except IncorrectMimeType as e:
-                    print e
-                    blacklist_add(e.uri)
-                except Exception as e:
-                    pass
+        #if len(BlackList.objects(ontology=o1)) <= 0 and len(BlackList.objects(ontology=o2)) <= 0 and o1 != o2:
+        for align_class in ALIGN_LIST:
+            #alignment = MongoAlign.objects(source_onto=o1, target_onto=o2, align_class=align_class)
+        #if len(alignment) <= 0:
+            #print 'Creating alignment between %s and %s using %s' % (o1, o2, align_class)
+            ap = None
+            ap = getattr(alignapy, align_class)()
+            ap_list.append((ap, o1, o2, align_class))
                 
+#result = map(process, ap_list)
+pool = multiprocessing.Pool(POOL_SIZE)
+result = pool.map(process, ap_list)         
+
 
 '''for key1 in graph_dict:
     g1 = graph_dict[key1]
